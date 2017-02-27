c0ba8a2a4804f58dfc5fb96b76ff39d08cc1c96e
lanefaison2 <faisonusmc@gmail.com>
2017-02-27 09:45:48 -0700

    update

--- public/edit.js
+++ public/edit.js
@@ -3,26 +3,31 @@ $(document).ready(function () {
 
   // window.location.search.split("&")
   // window.location = "/edit.html?=querystring"
-  // console.log(querystring)
-  // console.log(window.location);
+  // // console.log(querystring)
+  // // console.log(window.location);
 
-  // function getUrlParameter(sParam) {
-  //   const sPageURL = decodeURIComponent(window.location.search.substring(1));
-  //   const sURLVariables = sPageURL.split('&');
-  //   let returner;
-  //
-  //   sURLVariables.forEach((paraName) => {
-  //     const sParameterName = paraName.split('=');
-  //     if (sParameterName[0] === sParam) {
-  //       returner = sParameterName[1] === undefined ? true : sParameterName[1];
-  //     }
-  //   });
-  //   console.log(returner);
-  //   return returner;
-  // }
-  // getUrlParameter();
+  function getUrlParameter(sParam) {
+    const sPageURL = decodeURIComponent(window.location.search.substring(1));
+    const sURLVariables = sPageURL.split('&');
+    let returner;
+
+    sURLVariables.forEach((paraName) => {
+      const sParameterName = paraName.split('=');
+      if (sParameterName[0] === sParam) {
+        returner = sParameterName[1] === undefined ? true : sParameterName[1];
+      }
+    });
+    console.log(returner);
+    return returner;
+  }
+  var id = getUrlParameter('
+  id');
+  console.log(id);
+  // $.get('/movies/')
 
 })
+// Get to data on // ID
+
 
 
 
